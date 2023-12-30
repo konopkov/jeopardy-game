b5705cca74f7f07308b383ee8fac33135cde3873
Andrey Konopkov <konopkov@gmail.com>
2023-12-30 14:04:52 +0100

    fix: incorrect answer page refresh

--- components/question-view.tsx
+++ components/question-view.tsx
@@ -85,6 +85,7 @@ export const QuestionView = (props: QuestionViewProps) => {
     startTransition(() => {
       decrementScoreAction(gameId, answeringPlayerName, price);
       resetBuzzerAction(gameId);
+      redirect(playGameLink(gameId));
     });
   };
 
